3ee3dfbc5d1225984de68cf2d2d5fc9ca1448056
javier gonzalez <javier.gonzalezg@sngular.com>
2024-05-18 18:19:53 +0200

    test: add gamer head

---
+++ src/components/game-head/game-head.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { html, fixture, expect } from '@open-wc/testing';
+import './game-head-componet.js';
+
+describe('GameHead', () => {
+  it('should render player name correctly', async () => {
+    const playerName = 'John Doe';
+    const el = await fixture(
+      html`<game-head .playerName=${playerName}></game-head>`
+    );
+    const userInfoElement = el.shadowRoot.querySelector('.user-info');
+    expect(userInfoElement.textContent.trim()).to.equal(playerName);
+  });
+});
---
+++ src/memory-cards.test.js
@@ -0,0 +1,15 @@
+// import { fixture, expect } from '@open-wc/testing';
+// import './memory-cards.js'; // Importa el componente que quieres probar
+
+// describe('MemoryCards', () => {
+//   it('should render the outlet div', async () => {
+//     // Crea una instancia del componente
+//     const el = await fixture('<memory-cards></memory-cards>');
+
+//     // Busca el elemento outlet dentro del componente
+//     const outlet = el.shadowRoot.querySelector('#outlet');
+
+//     // Verifica que el elemento outlet existe
+//     expect(outlet).to.exist;
+//   });
+// });

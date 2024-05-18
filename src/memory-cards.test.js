7cfc7c16a963ff310f6e4de88aff8355f90215d9
javier gonzalez <javier.gonzalezg@sngular.com>
2024-05-18 19:46:28 +0200

    test: MemoryCards

--- src/memory-cards.test.js
+++ src/memory-cards.test.js
@@ -1,15 +1,26 @@
-// import { fixture, expect } from '@open-wc/testing';
-// import './memory-cards.js'; // Importa el componente que quieres probar
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions */
+import { html, fixture, expect } from '@open-wc/testing';
+import './memory-cards.js';
+import sinon from 'sinon';
+import { Router } from '@vaadin/router';
 
-// describe('MemoryCards', () => {
-//   it('should render the outlet div', async () => {
-//     // Crea una instancia del componente
-//     const el = await fixture('<memory-cards></memory-cards>');
+describe('MemoryCards', () => {
+  it('should render outlet element correctly', async () => {
+    const el = await fixture(html`<memory-cards></memory-cards>`);
+    const outlet = el.shadowRoot.querySelector('#outlet');
+    expect(outlet).to.exist;
+  });
 
-//     // Busca el elemento outlet dentro del componente
-//     const outlet = el.shadowRoot.querySelector('#outlet');
-
-//     // Verifica que el elemento outlet existe
-//     expect(outlet).to.exist;
-//   });
-// });
+  it('should set routes when first updated', async () => {
+    const routerStub = sinon.stub(Router.prototype, 'setRoutes');
+    const el = await fixture(html`<memory-cards></memory-cards>`);
+    await el.updateComplete;
+    expect(routerStub).to.have.been.calledWith([
+      { path: '/', component: 'home-view' },
+      { path: '/game', component: 'game-view' },
+      { path: '(.*)', component: 'home-view' },
+    ]);
+    routerStub.restore();
+  });
+});

73d23452efe44a6184bb220d59d10cf5dd14b630
javier gonzalez <javier.gonzalezg@sngular.com>
2024-05-18 19:37:07 +0200

    test: changes GameHead

--- src/components/game-head/game-head.test.js
+++ src/components/game-head/game-head.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions */
 import { html, fixture, expect } from '@open-wc/testing';
 import './game-head-componet.js';
+import sinon from 'sinon';
 
 describe('GameHead', () => {
   it('should render player name correctly', async () => {
@@ -10,5 +12,30 @@ describe('GameHead', () => {
     );
     const userInfoElement = el.shadowRoot.querySelector('.user-info');
     expect(userInfoElement.textContent.trim()).to.equal(playerName);
+  });
+
+  it('should emit a custom event when difficulty changes', async () => {
+    const el = await fixture(html`<game-head></game-head>`);
+    const selectElement = el.shadowRoot.querySelector('select');
+    const newDifficulty = 1;
+
+    // Espiamos la función dispatchEvent del documento
+    const dispatchEventSpy = sinon.spy(document, 'dispatchEvent');
+
+    // Simulamos el evento 'change' en el elemento select
+    const event = new Event('change');
+    selectElement.value = newDifficulty;
+    selectElement.dispatchEvent(event);
+
+    // Verificamos que dispatchEvent haya sido llamada con el evento personalizado
+    const changeLevelEvent = new CustomEvent('change:level', {
+      detail: { selectedDifficulty: newDifficulty },
+      bubbles: true,
+      cancelable: true,
+    });
+    expect(dispatchEventSpy.calledWith(changeLevelEvent)).to.be.true;
+
+    // Restauramos la función dispatchEvent original
+    dispatchEventSpy.restore();
   });
 });
